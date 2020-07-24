6e04854e4f9484c5a963d27bd474baccc956dfb0
fernandodmp <doritoz97@hotmail.com>
2020-07-23 21:39:28 -0300

    Corrects ORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { PeopleModule } from './people/people.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule, AppointmentsModule, PeopleModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AppointmentsModule,
+    PeopleModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,6 +2,6 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: __dirname + '/../../data',
+  database: __dirname + '/../../data/db.sqlite',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
 };
